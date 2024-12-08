281d223ccfc0df65455aec38d7c92f4827f78212
sam <samueljfry@protonmail.com>
2024-12-08 22:00:30 +0000

    get sectors

--- src/tests/test_x4/test_sectors/test_sectors.py
+++ src/tests/test_x4/test_sectors/test_sectors.py
@@ -5,10 +5,10 @@ import pytest
 
 @pytest.mark.django_db
 def test_get(create_basic_sector, client, django_user_model):
-    user = django_user_model.objects.create_user(username="test", password="test")
+    user = django_user_model.objects.create_user(
+        username="test", password="test"
+    )
     client.force_login(user)
     response = client.get("/sectors/")
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        "sectors": [{"name": "sector 001"}]
-    }
+    assert response.json() == {"sectors": [{"name": "sector 001"}]}
