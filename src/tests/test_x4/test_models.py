7c72935e822db2370c4c851f11fbd22d1578ba00
sam <samueljfry@protonmail.com>
2024-12-08 20:16:24 +0000

    model for sector

--- src/tests/test_x4/test_models.py
+++ src/tests/test_x4/test_models.py
@@ -3,15 +3,18 @@ import pytest
 from x4companion.x4.models import Sector
 
 
-@pytest.mark.django_db
-def test_create_sector():
+@pytest.fixture
+def create_sector():
     sector = Sector.objects.create(name="sector 001")
     sector.save()
+    return sector
+
+
+@pytest.mark.django_db
+def test_create_sector(create_sector):
     assert len(Sector.objects.all()) == 1
 
 
 @pytest.mark.django_db
-def test_sector_str():
-    sector = Sector.objects.create(name="sector 001")
-    sector.save()
-    assert str(sector) == "Sector sector 001"
+def test_sector_str(create_sector):
+    assert str(create_sector) == "Sector sector 001"
