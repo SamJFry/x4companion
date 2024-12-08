9264dc4cc2066a5eea0f18f5a31ddd3973dfc62e
sam <samueljfry@protonmail.com>
2024-12-08 23:11:49 +0000

    test for post

--- src/tests/test_x4/test_sectors/test_sectors.py
+++ src/tests/test_x4/test_sectors/test_sectors.py
@@ -3,12 +3,23 @@ from http import HTTPStatus
 import pytest
 
 
-@pytest.mark.django_db
-def test_get(create_basic_sector, client, django_user_model):
+@pytest.fixture
+def logged_in_client(client, django_user_model):
     user = django_user_model.objects.create_user(
         username="test", password="test"
     )
     client.force_login(user)
-    response = client.get("/sectors/")
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"sectors": [{"name": "sector 001"}]}
+    return client
+
+
+class TestSectors:
+    @pytest.mark.django_db
+    def test_get(self, create_basic_sector, logged_in_client):
+        response = logged_in_client.get("/sectors/")
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == {"sectors": [{"name": "sector 001"}]}
+
+    @pytest.mark.django_db
+    def test_post(self, logged_in_client):
+        response = logged_in_client.post("/sectors/", {"name": "test sector"})
+        assert response.status_code == HTTPStatus.CREATED
