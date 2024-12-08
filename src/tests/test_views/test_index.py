b0d563878569b12cc60c6a47c17f4c4c212e3ff2
sam <samueljfry@protonmail.com>
2024-12-08 17:43:48 +0000

    test index

--- src/tests/test_views/test_index.py
+++ src/tests/test_views/test_index.py
@@ -1,5 +1,13 @@
 import pytest
+from pytest_django.fixtures import client
+
+from x4companion import __version__
 
 
-def test_index():
-    pass
+def test_index(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "name": "X4 Companion App",
+        "version": __version__
+    }
--- src/x4companion/urls.py
+++ src/x4companion/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-import x4
+from x4companion import x4
 
 
 urlpatterns = [
