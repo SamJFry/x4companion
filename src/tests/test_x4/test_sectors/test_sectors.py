5da8ece545002787f1a21028bf2f47e391905560
sam <samueljfry@protonmail.com>
2024-12-09 17:22:59 +0000

    create sector

--- src/tests/test_x4/test_sectors/test_sectors.py
+++ src/tests/test_x4/test_sectors/test_sectors.py
@@ -1,6 +1,9 @@
 from http import HTTPStatus
 
 import pytest
+from django.forms.models import model_to_dict
+
+from x4companion.x4.models import Sector
 
 
 @pytest.fixture
@@ -23,3 +26,7 @@ class TestSectors:
     def test_post(self, logged_in_client):
         response = logged_in_client.post("/sectors/", {"name": "test sector"})
         assert response.status_code == HTTPStatus.CREATED
+        assert model_to_dict(Sector.objects.get(name="test sector")) == {
+            "id": 1,
+            "name": "test sector",
+        }
--- src/x4companion/x4/sectors/sectors.py
+++ src/x4companion/x4/sectors/sectors.py
@@ -1,7 +1,7 @@
 """Contains API views relating to sectors."""
 
-from django.http import HttpRequest
 from rest_framework import status
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,17 +12,33 @@ from x4companion.x4.serializers import SectorSerializer
 class Sectors(APIView):
     """Manage sectors."""
 
-    def get(self, request: HttpRequest) -> Response:
+    def get(self, request: Request) -> Response:
         """Get all sectors currently configured.
 
         Args:
-            request: Get request made to this endpoint.
+            request: GET request made to this endpoint.
 
         Returns:
             A JSON response containing a list of sectors and their attributes.
+
         """
         serializer = SectorSerializer(Sector.objects.all(), many=True)
         return Response(
             {"sectors": serializer.data},
             status=status.HTTP_200_OK,
         )
+
+    def post(self, request: Request) -> Response:
+        """Creates a new sector in the database.
+
+        Args:
+            request: POST request with the json to create a new sector.
+
+        Returns:
+            JSON Response detailing the objectst that have been created.
+
+        """
+        serializer = SectorSerializer(data=request.data)
+        serializer.is_valid()
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
