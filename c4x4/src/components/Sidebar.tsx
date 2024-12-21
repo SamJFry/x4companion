1511c55bcbc1100b5e1980efbb4b790b810fe4e2
sam <samueljfry@protonmail.com>
2024-12-21 16:02:13 +0000

    add a profile thing, it's not perfect but we're gettings somewhere

--- c4x4/src/components/Sidebar.tsx
+++ c4x4/src/components/Sidebar.tsx
@@ -7,8 +7,19 @@ import FactoryOutlinedIcon from '@mui/icons-material/FactoryOutlined';
 import ApartmentOutlinedIcon from '@mui/icons-material/ApartmentOutlined';
 import HouseOutlinedIcon from '@mui/icons-material/HouseOutlined';
 import LandslideOutlinedIcon from '@mui/icons-material/LandslideOutlined';
+import PopupState, {bindMenu, bindTrigger} from "material-ui-popup-state";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const NAVIGATION: Navigation = [
+  {kind: 'divider'},
+
+  {
+    kind: 'header',
+    title: 'Empire',
+  },
   {
     segment: 'sectors',
     title: 'Sectors',
@@ -25,27 +36,58 @@ const NAVIGATION: Navigation = [
     icon: <PrecisionManufacturingOutlinedIcon />
   },
   {
-    segment: 'factory-modules',
-    title: 'Factory Modules',
-    icon: <FactoryOutlinedIcon />,
-  },
-  {
     segment: 'habitats',
     title: 'Habitats',
     icon: <HouseOutlinedIcon />,
+  },
+  {kind: 'divider'},
+
+  {
+    kind: 'header',
+    title: 'Available Modules',
+  },
+
+  {
+    segment: 'factory-modules',
+    title: 'Factory Modules',
+    icon: <FactoryOutlinedIcon />,
   },
   {
     segment: 'habitat-modules',
     title: 'Habitat Modules',
     icon: <ApartmentOutlinedIcon />
   },
+  {kind: 'divider'},
+  {
+    kind: 'header',
+    title: 'Other',
+  },
   {
     segment: 'resources',
     title: 'Resources',
     icon: <LandslideOutlinedIcon />
-  }
+  },
+  {kind: 'divider'},
 ]
 
+function ProfileDropdown() {
+  return (
+    <PopupState variant="popover" popupId="demo-popup-menu">
+      {(popupState) => (
+        <React.Fragment>
+          <Button variant="contained" {...bindTrigger(popupState)}>
+            Dashboard
+          </Button>
+          <Menu {...bindMenu(popupState)}>
+            <MenuItem onClick={popupState.close}>Profile</MenuItem>
+            <MenuItem onClick={popupState.close}>My account</MenuItem>
+            <MenuItem onClick={popupState.close}>Logout</MenuItem>
+          </Menu>
+        </React.Fragment>
+      )}
+    </PopupState>
+  );
+}
 
 export default function X4Base() {
   return (
@@ -57,7 +99,9 @@ export default function X4Base() {
         homeUrl: '',
       }}
     >
-      <DashboardLayout />
+      <DashboardLayout
+        children={ProfileDropdown()}
+      ></DashboardLayout>
     </AppProvider>
   )
 }
