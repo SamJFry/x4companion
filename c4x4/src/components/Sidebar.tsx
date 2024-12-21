1dcff372ae99669b266584b35c183ea5a9ef01d2
sam <samueljfry@protonmail.com>
2024-12-21 15:39:16 +0000

    added icons to the sidebar for the different pages

--- c4x4/src/components/Sidebar.tsx
+++ c4x4/src/components/Sidebar.tsx
@@ -1,9 +1,62 @@
-import { AppProvider } from '@toolpad/core/AppProvider';
+import {AppProvider, Navigation} from '@toolpad/core/AppProvider';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
+import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
+import PrecisionManufacturingOutlinedIcon from '@mui/icons-material/PrecisionManufacturingOutlined';
+import FactoryOutlinedIcon from '@mui/icons-material/FactoryOutlined';
+import ApartmentOutlinedIcon from '@mui/icons-material/ApartmentOutlined';
+import HouseOutlinedIcon from '@mui/icons-material/HouseOutlined';
+import LandslideOutlinedIcon from '@mui/icons-material/LandslideOutlined';
+
+const NAVIGATION: Navigation = [
+  {
+    segment: 'sectors',
+    title: 'Sectors',
+    icon: <FlagOutlinedIcon />
+  },
+  {
+    segment: 'stations',
+    title: 'Stations',
+    icon: <PlaceOutlinedIcon />
+  },
+  {
+    segment: 'factories',
+    title: 'Factories',
+    icon: <PrecisionManufacturingOutlinedIcon />
+  },
+  {
+    segment: 'factory-modules',
+    title: 'Factory Modules',
+    icon: <FactoryOutlinedIcon />,
+  },
+  {
+    segment: 'habitats',
+    title: 'Habitats',
+    icon: <HouseOutlinedIcon />,
+  },
+  {
+    segment: 'habitat-modules',
+    title: 'Habitat Modules',
+    icon: <ApartmentOutlinedIcon />
+  },
+  {
+    segment: 'resources',
+    title: 'Resources',
+    icon: <LandslideOutlinedIcon />
+  }
+]
+
 
 export default function X4Base() {
   return (
-    <AppProvider>
+    <AppProvider
+      navigation={NAVIGATION}
+      branding={{
+        logo: <img src="/src/assets/X4.svg" alt="x4 logo"/>,
+        title: 'Companion',
+        homeUrl: '',
+      }}
+    >
       <DashboardLayout />
     </AppProvider>
   )
--- c4x4/src/main.tsx
+++ c4x4/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import X4Base from "./components/Sidebar.tsx";
 
 createRoot(document.getElementById('root')!).render(
