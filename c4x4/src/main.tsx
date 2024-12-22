4f7d7f256d0a340c76facb65c723ae1a71e92f13
sam <samueljfry@protonmail.com>
2024-12-22 22:32:39 +0000

    add routes to go between pages

--- c4x4/src/main.tsx
+++ c4x4/src/main.tsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import X4Base from "./components/Sidebar.tsx";
 import CredentialsSignInPage from "./components/SignIn.tsx";
 import './x4.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <CredentialsSignInPage />
-    <X4Base />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="login" element={<CredentialsSignInPage />} />
+        <Route path="app" element={<X4Base />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>
 )
