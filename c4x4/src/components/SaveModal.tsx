238a6ec0154e09501c0fff020ce94b74f5f3ce36
sam <samueljfry@protonmail.com>
2024-12-28 22:06:04 +0000

    crappy form implementation

--- c4x4/src/components/SaveModal.tsx
+++ c4x4/src/components/SaveModal.tsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from "react";
 import {Modal, Box, Typography, FormControl, InputLabel} from "@mui/material";
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
 import MenuItem from "@mui/material/MenuItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import AddIcon from "@mui/icons-material/Add";
@@ -13,7 +14,7 @@ const style = {
   top: '20%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: '70rem',
+  minWidth: 400,
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 4,
@@ -23,18 +24,6 @@ export function NewSaveModal() {
   const [isOpen, setOpen] = useState(true)
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
-  const [dataset, setDataset] = useState('');
-  const handleChange = (event: SelectChangeEvent) => {
-    setDataset(event.target.value);
-  };
-  const [datasets, setDatasets] = useState<Array<object[]>>([])
-  useEffect(() => {
-    const getData = async () => {
-      const data = await getDatasets()
-      setDatasets(data)
-    }
-    getData()
-  }, [])
   return (
     <>
       <MenuItem onClick={handleOpen}>
@@ -51,21 +40,51 @@ export function NewSaveModal() {
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Create New Save
           </Typography>
-          <FormControl size="small" sx={{ m: 1, minWidth: 200 }}>
-            <InputLabel id="dataset-select">Dataset</InputLabel>
-            <Select
-              labelId="dataset-select"
-              value={dataset}
-              label="Dataset"
-              onChange={handleChange}
-            >
-              {datasets.map((data: object) => (
-                <MenuItem value={data["id"]}>{data["name"]}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+          <DatasetForm/>
         </Box>
       </Modal>
+    </>
+  )
+}
+
+function DatasetForm() {
+  const [dataset, setDataset] = useState('');
+  const handleChange = (event: SelectChangeEvent) => {
+    setDataset(event.target.value);
+  };
+  const [datasets, setDatasets] = useState<Array<object[]>>([])
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getDatasets()
+      setDatasets(data)
+    }
+    getData()
+  }, [])
+  return (
+    <>
+      <TextField
+        className="left-0"
+        id="save-name"
+        variant="outlined"
+        label="Save Name"
+        type="text"
+        required
+        size="small"
+        sx={{ mt: 1 , minWidth: 400 }}
+      />
+      <FormControl required size="small" sx={{ m: 1, minWidth: 400 }}>
+        <InputLabel className="left-0" id="dataset-select">Dataset</InputLabel>
+        <Select
+          labelId="dataset-select"
+          value={dataset}
+          label="Dataset *"
+          onChange={handleChange}
+        >
+          {datasets.map((data: object) => (
+            <MenuItem value={data["id"]}>{data["name"]}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </>
   )
 }
