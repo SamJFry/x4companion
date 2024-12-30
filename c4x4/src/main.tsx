509565fa8a0f42ef7fa0c03dac61be6ab9d4afa9
sam <samueljfry@protonmail.com>
2024-12-30 16:54:15 +0000

    roots

--- c4x4/src/main.tsx
+++ c4x4/src/main.tsx
@@ -11,6 +11,13 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<CredentialsSignInPage />} />
         <Route path="app" element={<X4Base />} />
+        <Route path="sectors" element={<X4Base />} />
+        <Route path="stations" element={<X4Base />} />
+        <Route path="factories" element={<X4Base />} />
+        <Route path="habitats" element={<X4Base />} />
+        <Route path="factory-modules" element={<X4Base />} />
+        <Route path="habitat-modules" element={<X4Base />} />
+        <Route path="resources" element={<X4Base />} />
       </Routes>
     </StrictMode>
   </BrowserRouter>
