8bc6873c598a3c4e00e195da02cdccede6f72779
sam <samueljfry@protonmail.com>
2025-05-03 11:42:57 +0100

    datasetId always gets set now

--- c4x4/src/components/SaveIndicator.tsx
+++ c4x4/src/components/SaveIndicator.tsx
@@ -9,10 +9,12 @@ export function SaveIndicator({ saves }: SaveIndicatorProps) {
   const saveCookie = Number(getCookie('saveId'))
   if (saveCookie && saves.length !== 0) {
     const save = saves.find((element) => element.id === saveCookie)
+    setCookie('datasetId', save.dataset_id)
     return <Typography color="primary">Save: {save.name}</Typography>
   }
   if (saves.length !== 0) {
     setCookie('saveId', saves[0].id)
+    setCookie('datasetId', saves[0].dataset.id)
     return <Typography color="primary">Save: {saves[0]["name"]}</Typography>
   }
   return <Typography color="primary"> No Saves</Typography>
