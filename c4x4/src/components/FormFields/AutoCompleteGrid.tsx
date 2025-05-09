ae771361bfeec7347d94d670ccb4d380286ff143
sam <samueljfry@protonmail.com>
2025-05-09 17:24:11 +0100

    Organised my form field grid better

--- c4x4/src/components/FormFields/AutoCompleteGrid.tsx
+++ c4x4/src/components/FormFields/AutoCompleteGrid.tsx
@@ -1,26 +1,35 @@
-import { ReactNode, useState, useEffect } from 'react'
+import { ElementType, useState } from 'react'
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import Grid from "@mui/material/Grid";
+import {Typography} from "@mui/material";
+import { SxProps, Theme } from "@mui/material/styles";
 
 type FormFieldGridProps = {
-  field: ReactNode;
+  title: string
+  field: ElementType
+  sx?: SxProps<Theme>
 }
 
-export default function FormFieldGrid({ field }: FormFieldGridProps) {
+export default function FormFieldGrid({ title, field, sx }: FormFieldGridProps) {
   const [fieldCount, setFieldCount] = useState<number>(0)
   const FieldComponent = field;
 
   return <>
-    <Grid container spacing={2}>
+    <Grid container spacing={2} sx={sx}>
+      <Grid size={{lg: 2}}>
+        <Typography fontWeight="bold">{title}</Typography>
+      </Grid>
+      <Grid size={{lg: 10}}>
+        <Button variant="contained" onClick={() => setFieldCount(fieldCount + 1)}>
+          <AddIcon fontSize="small" />
+        </Button>
+      </Grid>
       {Array.from({ length: fieldCount }).map(() => (
         <Grid size={{ lg: 4, xs: 12 }}>
           <FieldComponent />
         </Grid>
       ))}
     </Grid>
-    <Button variant="contained" onClick={() => setFieldCount(fieldCount + 1)}>
-      <AddIcon fontSize="small" />
-    </Button>
   </>
 }
--- c4x4/src/components/Stations/AddStationForm.tsx
+++ c4x4/src/components/Stations/AddStationForm.tsx
@@ -11,6 +11,7 @@ export default function AddStationForm() {
   return (
     <OwnedSectorsProvider>
       <Typography variant="h4" sx={{mb: 2}}>Create Station</Typography>
+      <Typography fontWeight="bold">Location</Typography>
       <Grid container spacing={2} sx={{mb: 2}}>
         <Grid size={{ lg: 4, xs: 12 }}>
           <TextField id="outlined-basic" label="Name" variant="outlined" fullWidth />
@@ -19,8 +20,8 @@ export default function AddStationForm() {
           <SectorsAutoComplete />
         </Grid>
       </Grid>
-      <Typography fontWeight="bold">Factory Modules</Typography>
-      <FormFieldGrid field={SectorsAutoComplete} />
+      <FormFieldGrid title="Factory Modules" field={SectorsAutoComplete} sx={{mb: 2}}/>
+      <FormFieldGrid title="Habitat Modules" field={SectorsAutoComplete} />
     </OwnedSectorsProvider>
   )
 }
